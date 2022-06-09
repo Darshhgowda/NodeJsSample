14f07995630df6911b0d18cb9631ce0e7acc196d
BhavishyaRamesh <101257695+BhavishyaRamesh@users.noreply.github.com>
2022-06-09 10:21:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Success 
+          Hello
         </h1>
         <a
           className="App-link"
